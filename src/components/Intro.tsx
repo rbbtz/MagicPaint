1acbf03212a47c500734b7073eb3f49a8aed861e
rbbtz <robbetz@gmail.com>
2021-04-12 23:08:21 -0400

    up and running

---
+++ src/components/Intro.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface Props {
+  init?: any;
+  isReady?: boolean;
+}
+
+export const Intro: React.FC<Props> = ({ init, isReady }) => {
+  return (
+    <header className={isReady ? "hidden intro" : "intro"}>
+      <div className="intro__content">
+        <h1>MagicPaint</h1>
+        <button onClick={init} className="blob-btn">
+          <span className="blob-text">A masterpiece in the making</span>
+          <span className="blob-btn__inner">
+            <span className="blob-btn__blobs">
+              <span className="blob-btn__blob"></span>
+              <span className="blob-btn__blob"></span>
+              <span className="blob-btn__blob"></span>
+              <span className="blob-btn__blob"></span>
+            </span>
+          </span>
+        </button>
+        {
+        }
+      </div>
+    </header>
+  );
+};
